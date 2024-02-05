8744e2db5c7484199010ff1d2cbdcdfaded3d275
Rubens Teles Freire Junior <rteles-f@c2r12s1.42lisboa.com>
2024-02-05 17:49:13 +0000

    more bureaucrat work

--- Module05/ex00/Bureaucrat.cpp
+++ Module05/ex00/Bureaucrat.cpp
@@ -6,7 +6,7 @@
 /*   By: rteles-f <rteles-f@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/05 17:30:13 by rteles-f          #+#    #+#             */
-/*   Updated: 2024/02/05 17:32:43 by rteles-f         ###   ########.fr       */
+/*   Updated: 2024/02/05 17:47:00 by rteles-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,16 +17,47 @@ Bureaucrat::Bureaucrat():
 
 Bureaucrat::Bureaucrat(const Bureaucrat& tocopy)
 {
-    std::cout << "Bureaucrat Copy Constructor called." << std::endl;
-    *this = tocopy;
+	std::cout << "Bureaucrat Copy Constructor called." << std::endl;
+	*this = tocopy;
 }
 
 Bureaucrat::~Bureaucrat()
 { std::cout << "Bureaucrat Destructor called." << std::endl; }
 
 Bureaucrat& Bureaucrat::operator=(const Bureaucrat& tocopy) {
-	if (this != &tocopy)
-		;
+	if (this == &tocopy)
+		return (*this);
+	return (*this);
+}
 
-    return (*this);
+void	Bureaucrat::setGrade(int grade) {
+	if (grade < 1) {
+		// Bureaucrat::GradeTooLowException;
+		return ;
+	}
+	else if (grade > 150) {
+		//Bureaucrat::GradeTooHighException;
+		return ;
+	}
+	this->_grade = grade;
+}
+
+int	Bureaucrat::getGrade(void) {
+	return (this->_grade);
+}
+
+void	Bureaucrat::setName(int grade) {
+	if (grade < 1) {
+		// Bureaucrat::GradeTooLowException;
+		return ;
+	}
+	else if (grade > 150) {
+		//Bureaucrat::GradeTooHighException;
+		return ;
+	}
+	this->_grade = grade;
+}
+
+std::string	Bureaucrat::getName(void) {
+	return (this->_name);
 }
--- Module05/ex00/Bureaucrat.hpp
+++ Module05/ex00/Bureaucrat.hpp
@@ -6,7 +6,7 @@
 /*   By: rteles-f <rteles-f@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/05 17:30:16 by rteles-f          #+#    #+#             */
-/*   Updated: 2024/02/05 17:33:01 by rteles-f         ###   ########.fr       */
+/*   Updated: 2024/02/05 17:48:18 by rteles-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,21 @@
 # include <iostream>
 
 class Bureaucrat {
-    protected:
+	private:
+		std::string	_name;
+		int			_grade;
 
-    public:
-        Bureaucrat();
-        Bureaucrat(const Bureaucrat& tocopy);
-        ~Bureaucrat();
+	public:
+		Bureaucrat();
+		Bureaucrat(const Bureaucrat& tocopy);
+		~Bureaucrat();
 
-        Bureaucrat& operator=(const Bureaucrat& tocopy);
+		Bureaucrat& operator=(const Bureaucrat& tocopy);
+
+		int			getGrade(void) const;
+		void		setGrade(int grade);
+		std::string	getName(void) const;
+		void		setName(std::string name);
 } ;
 
 #endif
