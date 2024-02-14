8c87aa925fd34d4be057634a2f2d5c728e849de9
Rubens Teles Freire Junior <rteles-f@c2r6s3.42lisboa.com>
2024-02-14 17:01:47 +0000

    understanding the first exercise

--- Module06/ex00/ScalarConverter.cpp
+++ Module06/ex00/ScalarConverter.cpp
@@ -6,30 +6,78 @@
 /*   By: rteles-f <rteles-f@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/05 17:30:13 by rteles-f          #+#    #+#             */
-/*   Updated: 2024/02/14 14:47:56 by rteles-f         ###   ########.fr       */
+/*   Updated: 2024/02/14 17:00:10 by rteles-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ScalarConverter.hpp"
 
-ScalarConverter::ScalarConverter():
-{ std::cout << "ScalarConverter Default Constructor called." << std::endl; }
+ScalarConverter::ScalarConverter() {}
 
-ScalarConverter::ScalarConverter(const ScalarConverter& tocopy)
-{
-	std::cout << "ScalarConverter Copy Constructor called." << std::endl;
-	*this = tocopy;
-}
+ScalarConverter::ScalarConverter(const ScalarConverter& tocopy) {}
 
-ScalarConverter::~ScalarConverter()
-{ std::cout << "ScalarConverter Destructor called." << std::endl; }
+ScalarConverter::~ScalarConverter() {}
 
 ScalarConverter& ScalarConverter::operator=(const ScalarConverter& tocopy) {
-	if (this == &tocopy)
-		return (*this);
-	asdasd;
-	/*
-	to do;
-	*/
 	return (*this);
 }
+
+void	ScalarConverter::printChar(std::string convert)
+{
+	char	*print
+
+	if (convert.size() < 5) {
+		print
+	}
+	else
+	005
+}
+
+void	ScalarConverter::printInt(std::string convert) {
+	
+}
+
+void	ScalarConverter::printFloat(std::string convert) {
+	
+}
+
+void	ScalarConverter::printDouble(std::string convert) {
+	
+}
+
+template<typename T>
+void	printAll(T print) {
+	std::cout << static_cast<char>(print) << std::endl;
+	std::cout << static_cast<int>(print) << std::endl;
+	std::cout << static_cast<float>(print) << std::endl;
+	std::cout << static_cast<double>(print) << std::endl;	
+}
+
+void	ScalarConverter::convert(const std::string convert) {
+	double	converted;
+	char	test;
+	
+	std::cout << static_cast<char>(converted) << std::endl;
+	std::cout << static_cast<int>(converted) << std::endl;
+	std::cout << static_cast<float>(converted) << std::endl;
+	std::cout << static_cast<double>(converted) << std::endl;
+	printChar(convert);
+	printInt(convert);
+	printFloat(convert);
+	printDouble(convert);
+	
+	printAll(converted);
+	printAll(test);
+	
+	sizeof(double);
+	sizeof(int);
+	sizeof(float);
+	
+}
+
+/*
+char
+• int
+• float
+• double
+*/
--- Module06/ex00/ScalarConverter.hpp
+++ Module06/ex00/ScalarConverter.hpp
@@ -6,7 +6,7 @@
 /*   By: rteles-f <rteles-f@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/05 17:30:16 by rteles-f          #+#    #+#             */
-/*   Updated: 2024/02/14 14:47:29 by rteles-f         ###   ########.fr       */
+/*   Updated: 2024/02/14 15:58:54 by rteles-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,20 @@
 # define SCALARCONVERTER_HPP
 
 # include <iostream>
-# include <exception>
+# include <sstream>
 
 class ScalarConverter {
 	private:
-
-	public:
 		ScalarConverter();
 		ScalarConverter(const ScalarConverter& tocopy);
 		~ScalarConverter();
+		static void	printChar(std::string convert);
+		static void	printInt(std::string convert);
+		static void	printFloat(std::string convert);
+		static void	printDouble(std::string convert);
+
+	public:
+		static void	convert(const std::string convert);
 
 		ScalarConverter& operator=(const ScalarConverter& tocopy);
 } ;
