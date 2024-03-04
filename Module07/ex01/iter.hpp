d18c19d6bcbf1041b74ec7415d8b799e09749834
Rubens Teles Freire Junior <rteles-f@c2r6s2.42lisboa.com>
2024-03-04 20:07:43 +0000

    finished mod7/1

--- Module07/ex01/iter.hpp
+++ Module07/ex01/iter.hpp
@@ -1,35 +1,30 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   whatever.hpp                                       :+:      :+:    :+:   */
+/*   iter.hpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rteles-f <rteles-f@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/03/04 18:45:31 by rteles-f          #+#    #+#             */
-/*   Updated: 2024/03/04 19:37:27 by rteles-f         ###   ########.fr       */
+/*   Created: 2024/03/04 19:37:15 by rteles-f          #+#    #+#             */
+/*   Updated: 2024/03/04 19:58:43 by rteles-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef WHATEVER_HPP
-# define WHATEVER_HPP
+#ifndef ITER_HPP
+# define ITER_HPP
+
+# include <cstdio>
+# include <iostream>
 
 template<typename T>
-void    swap(T& first, T& second) {
-	T	hold;
-
-	hold = first;
-	first = second;
-	second = hold;
+void    iter(T *array, size_t length, void(*apply)(T&)){
+    for (size_t i = 0; i < length; i++)
+        apply(array[i]);
 }
 
 template<typename T>
-T       min(T& first, T& second) {
-	return ((first < second) ? first : second);
-}
-
-template<typename T>
-T       max(T& first, T& second) {
-	return ((first > second) ? first : second);
+void	templatePrint(T& variable) {
+    std::cout << variable;
 }
 
 #endif
---
+++ Module07/ex01/main.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include "iter.hpp"
+
+class Test {
+	public:
+		std::string name;
+		Test() {name = "none";};
+};
+
+void	intFunction(int& number) {
+	number++;
+}
+
+void	charFunction(char& byte) {
+	byte--;
+}
+
+void	testFunction(Test& change) {
+	change.name = "Raul";
+}
+
+void	classPrint(Test& print) {
+	std::cout << print.name;
+}
+
+int	main(void)
+{
+	int		int_a[5] = {1, 2, 3, 4, 5};
+	char	char_a[11] = "helloworld";
+	Test	test_a[15];
+
+	::iter(int_a, 5, ::templatePrint);
+	std::cout << std::endl;
+	::iter(int_a, 5, intFunction);
+	::iter(int_a, 5, ::templatePrint);
+	std::cout << std::endl;
+
+	::iter(char_a, 10, ::templatePrint);
+	std::cout << std::endl;
+	::iter(char_a, 10, charFunction);
+	::iter(char_a, 10, ::templatePrint);
+	std::cout << std::endl;
+
+	::iter(test_a, 15, ::classPrint);
+	std::cout << std::endl;
+	::iter(test_a, 15, testFunction);
+	::iter(test_a, 15, classPrint);
+	std::cout << std::endl;
+}
