8d1707c505764e3b6de66d72c14ea87affb1e428
Rubens Teles Freire Junior <rteles-f@c2r3s7.42lisboa.com>
2024-03-21 15:02:39 +0000

    s

--- Module09/ex00/BitcoinExchange.cpp
+++ Module09/ex00/BitcoinExchange.cpp
@@ -6,7 +6,7 @@
 /*   By: rteles-f <rteles-f@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/13 19:30:35 by rteles-f          #+#    #+#             */
-/*   Updated: 2024/03/15 19:36:12 by rteles-f         ###   ########.fr       */
+/*   Updated: 2024/03/15 19:52:31 by rteles-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,17 +47,17 @@ bool	BitcoinExchange::validDay(t_bitdate date) {
 		limit = 28 + leapYear(date.year);
 	else
 		limit = 30 + ((date.month > 7) ? ((date.month - 7) % 2) : (date.month % 2));
-	if ((date.day > 0 && date.day <= limit))
-		return (true);
-	return (false);
+	if (date.day < 0 || date.day > limit)
+		return (false);
+	return (true);
 }
 
 bool	BitcoinExchange::validDate(std::string line) {
 	t_bitdate	date;
 	char		end;
 
-	if (std::sscanf(line.c_str(), "%d-%d-%d%c",
-			&date.year, &date.month, &date.day, &end) != 4
+	if (std::sscanf(line.c_str(), "%d-%d-%d%c%c",
+			&date.year, &date.month, &date.day, &end, &end) != 4
 			|| date.year < 0 || date.month < 0 || date.month > 12
 			|| !validDay(date))
 		throw std::runtime_error(std::string("Error: bad input => ") + line);
--- Module09/ex00/main.cpp
+++ Module09/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: rteles-f <rteles-f@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/13 19:52:42 by rteles-f          #+#    #+#             */
-/*   Updated: 2024/03/15 19:11:32 by rteles-f         ###   ########.fr       */
+/*   Updated: 2024/03/15 19:41:52 by rteles-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,13 @@
 
 int main(int ac, char **av)
 {
+	BitcoinExchange	csv;
+
 	if (ac != 2) {
 		std::cerr << "Error: could not open file." << std::endl;
 		return (1);
 	}
-	(void)av;
-	BitcoinExchange	csv;
-	BitcoinExchange	input;
+
 	try {
 		csv.initDataBase("data.csv", ',');
 		csv.worthByDate(av[1], '|');
@@ -29,4 +29,6 @@ int main(int ac, char **av)
 	catch (std::exception& event) {
 		std::cout << event.what() << std::endl;
 	}
+
+	return (0);
 }
